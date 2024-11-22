b8c8ac33a784ba68c1a259c5cc1be0b8805c192b
bijay <bijay.subedi@vedastudios.com>
2024-11-22 15:01:52 +0545

    feat: added the image in home and metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
@@ -21,6 +22,13 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h1> Hello from home</h1>
+
+      <Image
+        src="/assets/testing_image.jpg"
+        alt="abcd"
+        width={200}
+        height={200}
+      />
       <ol>
         {links.map((item) => {
           return (
